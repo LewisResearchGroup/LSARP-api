abeed1ef6c3cb6ed8470b7282612c623d0b907d9
Soren Wacker <soerendip@gmail.com>
2021-11-30 14:01:20 -0700

    ..

--- LSARP/tools.py
+++ LSARP/tools.py
@@ -21,11 +21,14 @@ def age_to_age_group(x):
     else:
         return "80+"
 
-def add_date_features_from_datetime_col(df, date_col_name):
-    '''
+
+def add_date_features_from_datetime_col(
+    df, date_col_name, numeric=False, add_prefix=False
+):
+    """
     Takes a column with dtype pandas.datetime and extracts time features.
     The year, quarter, month, week, date (without time) and combined features.
-    
+
     Parameters
     ----------
     df - pandas.DataFrame
@@ -43,26 +46,39 @@ def add_date_features_from_datetime_col(df, date_col_name):
     YEAR_WEEK - str, format: "%4d-W%02d"
     YEAR_MONTH - str, format: "%4d-M%02d"
     YEAR_QUATER - str, format: "%4d-Q%1d"
-    '''
-    assert date_col_name in df.columns, '%s not in df.columns' %(date_col_name)
+    """
+    assert date_col_name in df.columns, "%s not in df.columns" % (date_col_name)
     assert df[date_col_name].dtype
-    df.loc[:, 'YEAR'] = df[date_col_name].dt.year
-    df.loc[:, 'MONTH'] = df[date_col_name].dt.month
-    df.loc[:, 'WEEK'] = df[date_col_name].dt.isocalendar().week
-    df.loc[:, 'DAYOFWEEK'] = df[date_col_name].dt.dayofweek
-    df.loc[:, 'DAY'] = df[date_col_name].dt.day
-    df.loc[:, 'DAYOFYEAR'] = df[date_col_name].dt.dayofyear    
-    df.loc[:, 'DATE'] = df[date_col_name].dt.date
-    df.loc[:, 'QUARTER'] = df[date_col_name].dt.quarter
-    df.loc[:, 'YEAR_DAY'] = df.YEAR +\
-        df.DAYOFYEAR.apply(lambda x: (x-1)/365)
-    df.loc[:, 'YEAR_WEEK'] = df.YEAR +\
-        df.WEEK.apply(lambda x: (x-1)/52)
-    df.loc[:, 'YEAR_MONTH'] = df.YEAR +\
-        df.MONTH.apply(lambda x: (x-1)/12)
-    df.loc[:, 'YEAR_QUARTER'] = df.YEAR +\
-        df.QUARTER.apply(lambda x: (x-1)/4)
+    prefix = f"{date_col_name}_" if add_prefix else ""
+    df.loc[:, f"{prefix}YEAR"] = df[date_col_name].dt.year
+    df.loc[:, f"{prefix}MONTH"] = df[date_col_name].dt.month
+    df.loc[:, f"{prefix}WEEK"] = df[date_col_name].dt.isocalendar().week
+    df.loc[:, f"{prefix}DAYOFWEEK"] = df[date_col_name].dt.dayofweek
+    df.loc[:, f"{prefix}DAY"] = df[date_col_name].dt.day
+    df.loc[:, f"{prefix}DAYOFYEAR"] = df[date_col_name].dt.dayofyear
+    df.loc[:, f"{prefix}DATE"] = df[date_col_name].dt.date
+    df.loc[:, f"{prefix}QUARTER"] = df[date_col_name].dt.quarter
+
+    if numeric:
+        df.loc[:, f"{prefix}YEAR_DAY"] = df.YEAR + df.DAYOFYEAR.apply(
+            lambda x: (x - 1) / 365
+        )
+        df.loc[:, f"{prefix}YEAR_WEEK"] = df.YEAR + df.WEEK.apply(
+            lambda x: (x - 1) / 52
+        )
+        df.loc[:, f"{prefix}YEAR_MONTH"] = df.YEAR + df.MONTH.apply(
+            lambda x: (x - 1) / 12
+        )
+        df.loc[:, f"{prefix}YEAR_QUARTER"] = df.YEAR + df.QUARTER.apply(
+            lambda x: (x - 1) / 4
+        )
+    else:
+        df.loc[:, f"{prefix}YEAR_DAY"] = df.YEAR + "-" + df.DAYOFYEAR
+        df.loc[:, f"{prefix}YEAR_WEEK"] = df.YEAR + "-" + df.WEEK
+        df.loc[:, f"{prefix}YEAR_MONTH"] = df.YEAR + "-" + df.MONTH
+        df.loc[:, f"{prefix}YEAR_QUARTER"] = df.YEAR + "-" + df.QUARTER
+
 
 def sort_df_by_row_count(df, axis=1):
     ndx = df.sum(axis=axis).sort_values(ascending=False).index
-    return df.reindex(ndx, axis=axis%1)
+    return df.reindex(ndx, axis=axis % 1)
